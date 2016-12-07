9502d89ff5af963d87a870f6fec637779e6314bd
Tomonori Hayashi <hyshtmnr6238@gmail.com>
2016-12-07 15:21:40 +0900

    add

---
+++ application.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  // フレームワーク関連パッケージ
+  "github.com/labstack/echo"
+  "github.com/labstack/echo/engine/standard"
+  "github.com/labstack/echo/middleware"
+
+  // データベース関連パッケージ
+  "database/sql"
+  _ "github.com/go-sql-driver/mysql"
+
+  // ディレクトリ
+  "./tool"
+  _ "./model"
+)
+
+func db_connect() *sql.DB {
+  db,err := sql.Open("mysql","root:tomonori@tcp(52.196.55.156:3306)/social_app")
+
+  if err != nil {
+    panic(err.Error())
+  }
+  return db
+}
+
+func main(){
+
+  e := echo.New()
+  db := db_connect()
+  defer db.Close()
+  // ミドルウェアの使用機能
+  e.Use(middleware.Logger())
+  e.Use(middleware.Recover())
+
+  // モデル
+  e.Get("/json",tool.Res_json())
+
+  //サーバー構築 ポート8000
+  e.Run(standard.New(":8000"))
+}
